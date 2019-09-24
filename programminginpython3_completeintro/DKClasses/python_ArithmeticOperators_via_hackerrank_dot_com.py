7295c968a2998195406a65b2b8cdfb4c450eb3ec
DavidKuangGitHub <davidkuang416@gmail.com>
2019-09-24 12:24:28 -0400

    Add a .python file
    This is to add a .python file to solve hackerrank.com problem

---
+++ programminginpython3_completeintro/DKClasses/python_ArithmeticOperators_via_hackerrank_dot_com.py
@@ -0,0 +1,5 @@
+import math, os, random, re, sys
+
+#Task: Read tow integers from STDIN and print three lines where: (1) 1st line contains the sum of the two numbers; (2) 2nd line contains the difference of the two numbers (first - second) (3) 3rd line contains the product of the two numbers. 
+#Example 3  2 the output should be 5 (3+2)  1 (3-2)  6(3*2)
+print("Python to implement Arithmetic Operators")
