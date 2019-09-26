72dbc522ede2e1ec68ee994a55e2506dedb76b6d
DavidKuangGitHub <davidkuang416@gmail.com>
2019-09-26 15:22:57 -0400

    updated
    This is to explain that X, Y and Z represents the dimension of a cuboid along with an integer N. The expected output should be a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i+j+k is not equal to N. Note: 0 <= i <=X; 0 <= j <=Y; 0 <= k <=Z.

--- programminginpython3_completeintro/DKClasses/py_ListComprehensions_viaHackerrank_com_ws.py
+++ programminginpython3_completeintro/DKClasses/py_ListComprehensions_viaHackerrank_com_ws.py
@@ -7,7 +7,7 @@
 #[[0,0,0], [0,0,1],[0,1,0],[1,0,0],[1,1,1]]
 #Another Example would be 2 2 2 2 each digit following by an enter
 
-print("Enter 4 integers with certain conditions: ")
+print("Enter 4 integers with certain conditions x+y+z <> n: ")
 x, y, z, n = int(input()), int(input()),int(input()),int(input())
 print("Results:")
 #print([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n])
