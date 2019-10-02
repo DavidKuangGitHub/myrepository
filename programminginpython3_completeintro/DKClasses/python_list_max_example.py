c5f968c729f027504119be7dc4f827b43ebb04c3
DavidKuangGitHub <davidkuang416@gmail.com>
2019-10-02 11:49:44 -0400

    Create python_list_max_example.py
    This is to create python_list_max_example.py

---
+++ programminginpython3_completeintro/DKClasses/python_list_max_example.py
@@ -0,0 +1,10 @@
+#python_list_max_example.py
+print("Here is the example(s):")
+#list1, list2 = [123, 'xyz', 'zara', 'abc'], [456, 700, 200]
+intList = [10, 150, 160, 180, 120, 105]
+print("(0) Within list [10, 150, 160, 180, 120, 105], The Maximum Value in this List is : ", max(intList))
+#list1 = [123, 'xyz', 'zara', 'abc']
+#print("(1) Within list [123, 'xyz', 'zara', 'abc'], The Maximum Value in this list1 is : ", max(list1))
+list2 = [456, 700, 200]
+print("Within list [456, 700, 200], The Maximum Value in this list1 is : ", max(list2))
+print("Completed")
