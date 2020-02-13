5f4660b99250f73ab29d2f0fad3853007e5c8ff7
DavidKuangGitHub <davidkuang416@gmail.com>
2020-02-13 16:20:55 -0500

    Commit 4:22pm

---
+++ programminginpython3_completeintro/worklaptop_python_scriptssinceFeb7th_2020/verify_removeduplicatesfromsortedarrays.py
@@ -0,0 +1,10 @@
+from programminginpython3_completeintro.worklaptop_python_scriptssinceFeb7th_2020.leetcode_test_removeduplicatesfromsortedarray import SolutionFeb13c2020
+
+def main():
+    print("Verify class removeduplicatesfromsortedarrays.. now inside method main")
+    mySolutionFeb13c2020 = SolutionFeb13c2020()
+    myNumbers = [0,0,1,1,1,2,2,3,3,4]   #[1,1,2]
+    print(mySolutionFeb13c2020.removeDuplicates(myNumbers))
+
+if __name__ == '__main__':
+    main()
