3cc63b90920bb3865fc7480f9e42029c49a91d68
DavidKuangGitHub <davidkuang416@gmail.com>
2020-02-13 11:55:33 -0500

    commitFeb132020

---
+++ programminginpython3_completeintro/worklaptop_python_scriptssinceFeb7th_2020/needMyClass_after_git_pull.py
@@ -0,0 +1,12 @@
+from programminginpython3_completeintro.worklaptop_python_scriptssinceFeb7th_2020.test_git_PythonClass import \
+    MyClass02132020
+
+
+def main():
+    print("This step is inside method main.")
+    myInstanceOfMyClass = MyClass02132020()
+    print(myInstanceOfMyClass.x)
+
+
+if __name__ == '__main__':
+    main()
