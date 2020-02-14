82e0be951a9e7b7683071c1b4c2769def5fc77d8
DavidKuangGitHub <davidkuang416@gmail.com>
2020-02-14 10:38:49 -0500

    create conftest.py
    The purpose of this python fixture is to demo of conftest.py

---
+++ programminginpython3_completeintro/worklaptop_python_scriptssinceFeb7th_2020/conftest.py
@@ -0,0 +1,13 @@
+'''conftest.py
+'''
+
+import os, shutil, tempfile, pytest
+
+@pytest.fixture
+def cleandir():
+  old_cwd = os.getcwd()
+  newpath = tempfile.mkdtemp()
+  os.chdir(newpath)
+  yield
+  os.chdir(old_cwd)
+  shutil.rmtree(newpath)
