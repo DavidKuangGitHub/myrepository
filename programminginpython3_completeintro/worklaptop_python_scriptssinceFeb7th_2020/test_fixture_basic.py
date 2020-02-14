e93baf9ce3b22b5f503ecf064749f6e063b33b8f
DavidKuangGitHub <davidkuang416@gmail.com>
2020-02-14 09:07:33 -0500

    Create test_fixture_basic.py
    The purpose of this fixture is to implement/demo a happy-path of python fixture.

---
+++ programminginpython3_completeintro/worklaptop_python_scriptssinceFeb7th_2020/test_fixture_basic.py
@@ -0,0 +1,14 @@
+'''test_fixture_basic.py
+'''
+import pytest
+
+@pytest.fixture()
+def before():
+  print("\nBefore each test")
+  
+def test_1(before):
+  print("test_1()")
+  
+def test_2(before):
+  print("test_2")
+  assert 0
