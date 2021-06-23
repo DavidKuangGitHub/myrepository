dbd34e055956af696e47da8a8a03f910349f3b1c
DavidKuangGitHub <davidkuang416@gmail.com>
2021-06-23 10:29:42 -0400

    c# cSharp Program
    This is to take initiative of c# related solution

---
+++ dearborn_Jun23_2019/c#_cSharp_Program_otherthanSolutionFolder/program.cs
@@ -0,0 +1,11 @@
+using System;
+ 
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World on Jun 23 2021!");
+        }
+    }
+}
